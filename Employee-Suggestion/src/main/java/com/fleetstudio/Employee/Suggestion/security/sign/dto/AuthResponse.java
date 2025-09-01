3a83004dc4ff24f739e71b58abf963ff399969a2
Tharun-raj-u <traj@fleetstudio.com>
2025-09-01 14:54:14 +0530

    Removed hardcoded secrets

--- Employee-Suggestion/src/main/java/com/fleetstudio/Employee/Suggestion/security/sign/dto/AuthResponse.java
+++ Employee-Suggestion/src/main/java/com/fleetstudio/Employee/Suggestion/security/sign/dto/AuthResponse.java
@@ -8,4 +8,25 @@ import lombok.Data;
 public class AuthResponse {
     private String token;
     private String role;
+
+    public String getToken() {
+        return token;
+    }
+
+    public AuthResponse(String token, String role) {
+        this.token = token;
+        this.role = role;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
